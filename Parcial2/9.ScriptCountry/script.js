892f9c1fee3c374134fbf5a17df4fea4661a02d8
22100194-RaulFGM <guevaramunozraulfelipe@gmail.com>
2025-04-02 00:34:05 -0500

    Primer Commit

---
+++ Parcial2/9.ScriptCountry/script.js
@@ -0,0 +1,59 @@
+const localidades = {
+    "localidad 1": {
+        "Continente": "África",
+        "País": "Angola",
+        "Capital": "Luanda"
+    },
+    "localidad 2": {
+        "Continente": "América do Norte",
+        "País": "Estados Unidos",
+        "Capital": "Washington DC"
+    },
+    "localidad 3": {
+        "Continente": "América Central",
+        "País": "México",
+        "Capital": "Cidade do México"
+    },
+    "localidad 4": {
+        "Continente": "América do Sul",
+        "País": "Brasil",
+        "Capital": "Brasília"
+    },
+    "localidad 5": {
+        "Continente": "Europa",
+        "País": "Espanha",
+        "Capital": "Madri"
+    },
+    "localidad 6": {
+        "Continente": "Europa",
+        "País": "Alemanha",
+        "Capital": "Berlim"
+    },
+    "localidad 7": {
+        "Continente": "Oceania",
+        "País": "Austrália",
+        "Capital": "Camberra"
+    },
+    "localidad 8": {
+        "Continente": "Ásia",
+        "País": "Japão",
+        "Capital": "Tóquio"
+    }
+};
+
+function mostrarDatosLocalidad(localidad) {
+
+    if (localidades[localidad]) {
+        const datos = localidades[localidad];
+        console.log(`Datos de ${localidad}:`);
+        console.log(`Continente: ${datos.Continente}`);
+        console.log(`País: ${datos["País"]}`);
+        console.log(`Capital: ${datos.Capital}`);
+    } else {
+        console.log("La localidad ingresada no existe.");
+    }
+}
+
+const localidadUsuario = prompt("Ingrese la localidad: ");
+
+mostrarDatosLocalidad(localidadUsuario);
